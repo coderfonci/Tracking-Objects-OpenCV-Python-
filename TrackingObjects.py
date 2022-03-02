3775e450676283a04b89e63e991d2f3f93459bfc
sargent_ramzi <87293845+coderfonci@users.noreply.github.com>
2022-03-02 19:48:25 +0900

    Add files via upload

---
+++ TrackingObjects.py
@@ -0,0 +1,29 @@
+import cv2 
+
+cap = cv2.VideoCapture(0)
+
+tracker = cv2.TrackerMOSSE_create()
+success, img  = cap.read()
+bbox = cv2.selectROI('tracker',img,False)
+tracker.init(img,bbox)
+
+def drowbox(): 
+    x_,y_,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.putText(img,"lost",(75,75),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+while True:
+    timer = cv2.getTickCount()
+    success,bbox =  tracker.update(img)
+    
+    if  success: 
+        drowbox()
+    else: 
+        cv2.putText(img,"lost",(75,75),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,255),2)
+
+
+    fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+    cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+    cv2.imshow("Tracking", img)
+
+    if cv2.waitKey(1) & 0xff ==ord('q'):
+        break 
